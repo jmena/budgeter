20f62c3db9de846b370041ffed2fd08692618a8c
Javier Mena <javimena@gmail.com>
2016-02-12 01:37:49 -0800

    determine URL based on the information provided by GAE

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ run on intellij
 # view dependencies
 
     mvn versions:display-dependency-updates
+
+# upload to GAE
+
+    mvn appengine:update
--- src/main/java/me/bgx/budget/web/RootController.java
+++ src/main/java/me/bgx/budget/web/RootController.java
@@ -7,6 +7,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import com.google.appengine.api.utils.SystemProperty;
+import com.google.apphosting.api.ApiProxy;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,6 +17,20 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping(value = "/")
 public class RootController {
 
+    private static final String LOGIN_URL;
+    private static final String LOGOUT_URL;
+    static {
+        String hostName = "localhost";
+        if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
+            ApiProxy.Environment env = ApiProxy.getCurrentEnvironment();
+            hostName = (String) env.getAttributes().get("com.google.appengine.runtime.default_version_hostname");
+        } else if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Development) {
+            hostName = "localhost:8080";
+        }
+        LOGIN_URL = "http://" + hostName + "/app/rules";
+        LOGOUT_URL = "http://" + hostName + "/";
+    }
+
     @RequestMapping(method = RequestMethod.GET)
     public String welcome() {
         return "root/welcome";
@@ -22,12 +38,12 @@ public class RootController {
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView login() {
-        return processLogInOut(true, "http://localhost:8080/app/rules");
+        return processLogInOut(true, LOGIN_URL);
     }
 
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public ModelAndView logout() {
-        return processLogInOut(false, "http://localhost:8080/");
+        return processLogInOut(false, LOGOUT_URL);
     }
 
     private ModelAndView processLogInOut(final boolean isLogin, final String gotoUrl) {
